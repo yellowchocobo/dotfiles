f863320ef930573e70234536c5f61a4ac0486e18
Nils C. Prieur <38788919+yellowchocobo@users.noreply.github.com>
2021-05-14 17:51:49 +0200

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -70,3 +70,6 @@ sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+# slack
+sudo snap install slack --classic
